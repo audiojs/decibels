dae89f9b64278b3ad43ca040f445561e55507616
Matt McKegg <matt@wetsand.co.nz>
2017-12-07 10:58:11 +1300

    test: round conversion test down to 4 dp to pass on weird v8 implementations

--- test/conversions.js
+++ test/conversions.js
@@ -3,17 +3,22 @@ var dbToGain = require('../to-gain')
 var gainToDb = require('../from-gain')
 
 test('test gain -> db conversions', function (t) {
-  t.equal(gainToDb(0), -Infinity)
-  t.equal(gainToDb(1), 0)
-  t.equal(gainToDb(2), 6.020537780907573)
-  t.equal(gainToDb(0.5), -6.020537780907573)
+  t.equal(round(gainToDb(0)), -Infinity)
+  t.equal(round(gainToDb(1)), 0)
+  t.equal(round(gainToDb(2)), 6.0205)
+  t.equal(round(gainToDb(0.5)), -6.0205)
   t.end()
 })
 
 test('test db -> gain conversions', function (t) {
-  t.equal(dbToGain(0), 1)
-  t.equal(dbToGain(-Infinity), 0)
-  t.equal(dbToGain(-6), 0.5011836607504527)
-  t.equal(dbToGain(6), 1.9952765389490936)
+  t.equal(round(dbToGain(0)), 1)
+  t.equal(round(dbToGain(-Infinity)), 0)
+  t.equal(round(dbToGain(-6)), 0.5012)
+  t.equal(round(dbToGain(6)), 1.9953)
   t.end()
 })
+
+function round (value) {
+  var pow = Math.pow(10, 4)
+  return Math.round(value * pow) / pow
+}
